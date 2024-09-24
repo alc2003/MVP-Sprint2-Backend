2ea119b97c686b4881bf841259c79d2cf47ebb77
andre <alc2003@gmail.com>
2024-09-23 20:15:15 -0400

    ultimo commit

--- app.py
+++ app.py
@@ -28,24 +28,6 @@ def home():
     """
     return redirect('/openapi')
 
-
-@app.get('/consulta_cep/<cep>', tags=[paciente_tag], responses={"200": CepSchema, "400": ErrorSchema})
-def consulta_cep(cep: str):
-    """Consulta o endereço com base no CEP informado usando a API ViaCEP."""
-    try:
-        response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
-        data = response.json()
-
-        if "erro" in data:
-            return jsonify({"message": "CEP não encontrado"}), 400
-
-        # Retorna a resposta mapeada pelo modelo ViaCEPResponse
-        return jsonify(CepSchema(ViaCEPResponse(**data)).dict()), 200
-
-    except Exception as e:
-        return jsonify({"message": "Erro ao consultar o CEP"}), 400
-    
-
 @app.post('/adiciona_paciente', tags=[paciente_tag],
           responses={"200":  PacienteViewSchema, "409": ErrorSchema, "400": ErrorSchema})
 def add_paciente(form: PacienteSchema):
